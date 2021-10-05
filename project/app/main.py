a1df2c90fb2e93b20cf3d4513ac2358ea076dcb0
Juan David Garavito <jdgaravito317@gmail.com>
2021-10-05 08:52:23 -0500

    pre alembic

--- project/app/main.py
+++ project/app/main.py
@@ -1,14 +1,10 @@
 from fastapi import FastAPI
 from app.api import portfolio_api
-from app.db import init_db
 
 app = FastAPI()
 #Invoke the routes
 app.include_router(portfolio_api.router)
 
-@app.on_event("startup")
-async def on_startup():
-    await init_db()
 
 
 @app.get("/ping", include_in_schema=False)
